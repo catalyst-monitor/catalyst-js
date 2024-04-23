10e91ea6df1b605138ce58258e6eaf49b1e12fd7
Bill Liu <liu_bill@live.com>
2024-04-22 19:13:16 -0700

    [Next.JS] Fix annoying lint issues in sandbox package.

--- packages/nextjs/test/app/err/layout-err/layout.tsx
+++ packages/nextjs/test/app/err/layout-err/layout.tsx
@@ -1,5 +1,5 @@
 export const dynamic = 'force-dynamic'
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout() {
   throw new Error('Example of an error in layout!')
 }
